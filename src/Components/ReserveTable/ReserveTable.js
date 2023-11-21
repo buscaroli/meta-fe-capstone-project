bd5ed4926e4e315ea089a8d29292a4e28fe7bae0
buscaroli <m.buscaroli@outlook.com>
2023-11-21 17:05:46 +0000

    complete the ReserveTable Component and sub-components

--- src/Components/ReserveTable/ReserveTable.js
+++ src/Components/ReserveTable/ReserveTable.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import styles from './Reserve.module.css'
 import UserDetails from '../UserDetails/UserDetails'
+import UserPreferences from '../UserPreferences/UserPreferences'
 
 function ReserveTable() {
   return (
     <section className={styles.container}>
       <h5 className={styles.title}>Reserve a Table</h5>
-      {/* <div className={styles.userDetails}> */}
-      <UserDetails />
-      {/* </div> */}
-
-      <div className="userPreferences">User Preferences</div>
+      <form className={styles.form} action="">
+        <UserDetails />
+        <UserPreferences />
+      </form>
+      <button className={styles.reserveButton}>Reserve a Table</button>
     </section>
   )
 }
