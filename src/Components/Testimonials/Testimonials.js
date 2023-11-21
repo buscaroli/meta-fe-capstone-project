d2af32742337a782c46b7226a30b5eab9c65e70f
buscaroli <m.buscaroli@outlook.com>
2023-11-21 12:37:43 +0000

    finish styling the Review component, use the faker API to get the data

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -1,35 +1,60 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import Review from '../Review/Review'
 import styles from './Testimonials.module.css'
+import { faker } from '@faker-js/faker'
 
 function Testimonials() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    let dataArray = []
+    let firstName = ''
+    let picUrl = ''
+    let review = ''
+    let rating = ''
+
+    for (let i = 0; i < 3; i++) {
+      firstName = faker.person.firstName()
+      picUrl = faker.image.avatar()
+      rating = Math.floor(Math.random() * 5) + 1
+      review = faker.word.words({ count: { min: 15, max: 40 } })
+      dataArray.push({ firstName, picUrl, rating, review })
+    }
+    setData(dataArray)
+  }, [])
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
   return (
     <section className={styles.container}>
       <h5 className={styles.sectionTitle}>Testimonials</h5>
       <div className={styles.reviews}>
         <div className={styles.review1}>
           <Review
-            name="George"
-            picture=""
-            rating="5"
-            review="Great food and staff, I really enjoyed my meal and will definetely come back."
+            name={data[0].firstName}
+            picture={data[0].picUrl}
+            rating={data[0].rating}
+            review={data[0].review}
           />
         </div>
 
         <div className={styles.review2}>
           <Review
-            name="George"
-            picture=""
-            rating="5"
-            review="Great food and staff, I really enjoyed my meal and will definetely come back."
+            name={data[1].firstName}
+            picture={data[1].picUrl}
+            rating={data[1].rating}
+            review={data[1].review}
           />
         </div>
         <div className={styles.review3}>
           <Review
-            name="George"
-            picture=""
-            rating="5"
-            review="Great food and staff, I really enjoyed my meal and will definetely come back."
+            name={data[2].firstName}
+            picture={data[2].picUrl}
+            rating={data[2].rating}
+            review={data[2].review}
           />
         </div>
       </div>
