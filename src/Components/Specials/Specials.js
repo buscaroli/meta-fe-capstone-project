a56d174151ac4ab2c6641337d8ed076d6dd82347
buscaroli <m.buscaroli@outlook.com>
2023-11-21 10:26:49 +0000

    convert the Meal component from flex to grid to have the elements position consistently with the other components

--- src/Components/Specials/Specials.js
+++ src/Components/Specials/Specials.js
@@ -15,24 +15,32 @@ function Specials() {
         </div>
       </div>
       <div className={styles.reviews}>
-        <Meal
-          title="Greek Salad"
-          price="12.99"
-          picture={bruschetta}
-          description="The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. "
-        />
-        <Meal
-          title="Bruschetta"
-          price="5.99"
-          picture={salad}
-          description="Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. "
-        />
-        <Meal
-          title="Lemon Dessert"
-          price="5.00"
-          picture={dessert}
-          description="This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined."
-        />
+        <div className={styles.meal1}>
+          <Meal
+            title="Greek Salad"
+            price="12.99"
+            picture={bruschetta}
+            description="The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. "
+          />
+        </div>
+
+        <div className={styles.meal2}>
+          <Meal
+            title="Bruschetta"
+            price="5.99"
+            picture={salad}
+            description="Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. "
+          />
+        </div>
+
+        <div className={styles.meal3}>
+          <Meal
+            title="Lemon Dessert"
+            price="5.00"
+            picture={dessert}
+            description="This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined."
+          />
+        </div>
       </div>
     </section>
   )
