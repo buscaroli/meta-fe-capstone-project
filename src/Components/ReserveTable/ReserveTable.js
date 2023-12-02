72812f1389bb6cc84baf6d7674e6d189f21f28ec
buscaroli <m.buscaroli@outlook.com>
2023-12-02 17:35:36 +0000

    implement basics of table booking with hardcoded data through the useReducer Hook

--- src/Components/ReserveTable/ReserveTable.js
+++ src/Components/ReserveTable/ReserveTable.js
@@ -1,13 +1,81 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useReducer } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styles from './Reserve.module.css'
 import ReserveTableDetails from '../ReserveTableDetails/ReserveTableDetails'
+
+const availableSlots = {
+  am12: 10,
+  am13: 10,
+  am14: 10,
+  pm17: 10,
+  pm18: 10,
+  pm19: 10,
+  pm20: 10,
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'book-am12':
+      if (state.am12 === 0) {
+        console.log('Fully Booked')
+      } else {
+        return { ...state, am12: state.am12 - 1 }
+      }
+      break
+    case 'book-am13':
+      if (state.am13 === 0) {
+        console.log('Fully Booked')
+      } else {
+        return { ...state, am13: state.am13 - 1 }
+      }
+      break
+
+    case 'book-am14':
+      if (state.am14 === 0) {
+        console.log('Fully Booked')
+      } else {
+        return { ...state, am14: state.am14 - 1 }
+      }
+      break
+    case 'book-pm17':
+      if (state.pm17 === 0) {
+        console.log('Fully Booked')
+      } else {
+        return { ...state, pm17: state.pm17 - 1 }
+      }
+      break
+    case 'book-pm18':
+      if (state.pm18 === 0) {
+        console.log('Fully Booked')
+      } else {
+        return { ...state, pm18: state.pm18 - 1 }
+      }
+      break
+    case 'book-pm19':
+      if (state.pm19 === 0) {
+        console.log('Fully Booked')
+      } else {
+        return { ...state, pm19: state.pm19 - 1 }
+      }
+      break
+    case 'book-pm20':
+      if (state.pm20 === 0) {
+        console.log('Fully Booked')
+      } else {
+        return { ...state, pm20: state.pm20 - 1 }
+      }
+      break
+    default:
+      return state
+  }
+}
 
 function ReserveTable() {
   const [today, setToday] = useState('')
   const [timeNow, setTimeNow] = useState('')
   const [reservationData, setReservationData] = useState({})
+  const [bookings, dispatch] = useReducer(reducer, availableSlots)
 
   useEffect(() => {
     const date = new Date()
@@ -18,6 +86,8 @@ function ReserveTable() {
 
     const nowTime = String(`${date.getHours()}:${date.getMinutes()}}`)
     setTimeNow(nowTime)
+
+    console.log('ReerveTable - bookings: ', bookings)
   }, [])
 
   const navigate = useNavigate()
@@ -40,6 +110,8 @@ function ReserveTable() {
           fromDate={today}
           fromTime={timeNow}
           onReservationSubmit={onReservationSubmit}
+          bookings={bookings}
+          dispatch={dispatch}
         />
       </form>
     </section>
